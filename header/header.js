500067e96a0f017a65ce62eaa5bbfb8eb5a3db42
daniel <danielsrbu@gmail.com>
2025-04-01 19:13:26 -0600

    act

--- header/header.js
+++ header/header.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    console.log("entre");
-    // Crear el header dinámicamente
-    let header = document.createElement("header");
-    header.classList.add("text-center");
+console.log("entre");
+// Crear el header dinámicamente
+let header = document.createElement("header");
+header.classList.add("text-center");
 
-    let img = document.createElement("img");
-    img.src = "https://danieldaniel123456789.github.io/aeris/aeris.png";  // Ruta completa de la imagen
-    img.alt = "Logo";
+let img = document.createElement("img");
+img.src = "https://danieldaniel123456789.github.io/aeris/aeris.png"; // Ruta completa de la imagen
+img.alt = "Logo";
 
-    // Agregar la imagen al header
-    header.appendChild(img);
+// Agregar la imagen al header
+header.appendChild(img);
 
-    // Insertar el header en el contenedor
-    document.getElementById("header-container").appendChild(header);
+// Insertar el header en el contenedor
+document.getElementById("header-container").appendChild(header);
 });
